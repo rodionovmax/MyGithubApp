722db2991c8932f1c8334c98771365fa7a143d0e
Rodionovmax <mrodionov@connectrn.com>
2022-06-18 19:59:29 -0700

    added users entity, repo and FakeRepoImpl

---
+++ app/src/main/java/com/rodionovmax/mygithubapp/data/FakeUsersRepoImpl.kt
@@ -0,0 +1,24 @@
+package com.rodionovmax.mygithubapp.data
+
+import android.os.Handler
+import android.os.Looper
+import com.rodionovmax.mygithubapp.domain.entity.UserEntity
+import com.rodionovmax.mygithubapp.domain.repo.UsersRepo
+
+private const val DATA_LOADING_FAKE_DELAY = 3_000L
+class FakeUsersRepoImpl : UsersRepo {
+
+    val data: List<UserEntity> = listOf(
+        UserEntity("ivey", 6, "https://avatars.githubusercontent.com/u/6?v=4"),
+        UserEntity("evanphx", 7, "https://avatars.githubusercontent.com/u/7?v=4"),
+        UserEntity("vanpelt", 17, "https://avatars.githubusercontent.com/u/17?v=4"),
+        UserEntity("wayneeseguin", 18, "https://avatars.githubusercontent.com/u/18?v=4"),
+        UserEntity("brynary", 19, "https://avatars.githubusercontent.com/u/19?v=4")
+    )
+
+    override fun getUsers(onSuccess: (List<UserEntity>) -> Unit, onError: ((Throwable) -> Unit)?) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            onSuccess(data)
+        }, DATA_LOADING_FAKE_DELAY)
+    }
+}
---
+++ app/src/main/java/com/rodionovmax/mygithubapp/domain/entity/UserEntity.kt
@@ -0,0 +1,7 @@
+package com.rodionovmax.mygithubapp.domain.entity
+
+data class UserEntity(
+    val username: String,
+    val id: Long,
+    val userImg: String
+)
